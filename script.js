f1a4674985a7153400faafdd779444853b9f8279
Tyrone Williams <Tyronemwilliams@outlook.com>
2022-06-15 22:18:42 +0100

    light and dark theme btns

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 console.log(
   window
     .getComputedStyle(document.documentElement)
-    .getPropertyValue("--text-color")
+    .getPropertyValue("--div-background-color")
 );
+
+document.getElementById("dark-theme").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "#333");
+});
+
+document.getElementById("light-theme").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "#afafaf");
+});
